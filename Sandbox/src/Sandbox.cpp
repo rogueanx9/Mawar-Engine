a2f5a44cada7783737eea5bd4db40ddfaf506999
rogansteeven <rogansteeven@gmail.com>
2022-08-11 19:47:13 +0700

    Add Transform for Object

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -2,11 +2,13 @@
 
 #include "imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class LayerExample : public Mawar::Layer
 {
 public:
 	LayerExample()
-		: Layer("Example"), m_Camera(-2.0f, 2.0f, -1.5f, 1.5f), m_CameraPosition(0.0f)
+		: Layer("Example"), m_Camera(-2.0f, 2.0f, -1.5f, 1.5f), m_CameraPosition(0.0f), m_TransformPosition(0.0f)
 	{
 		/// <summary>
 		/// OpenGL Rendering Object
@@ -66,6 +68,7 @@ public:
              layout(location=1) in vec4 a_Color;
 
              uniform mat4 u_ViewProjection;
+             uniform mat4 u_Transform;
 
              out vec3 v_Position;
              out vec4 v_Color;
@@ -74,7 +77,7 @@ public:
              {
                   v_Position = a_Position;
                   v_Color = a_Color;
-                  gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+                  gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
              }
         )";
 
@@ -112,23 +115,44 @@ public:
 		if (Mawar::Input::IsKeyPressed(M_KEY_RIGHT))
 			m_CameraPosition.x += m_CameraMoveSpeed * ts;
 
-		if (Mawar::Input::IsKeyPressed(M_KEY_A))
+		if (Mawar::Input::IsKeyPressed(M_KEY_COMMA))
 			m_CameraRotation += m_CameraRotationSpeed * ts;
 
-		if (Mawar::Input::IsKeyPressed(M_KEY_D))
+		if (Mawar::Input::IsKeyPressed(M_KEY_APOSTROPHE))
 			m_CameraRotation -= m_CameraRotationSpeed * ts;
 
-		glm::vec4 clear_color = { 0.2f,0.2f,0.2f,1.0f };
+		if (Mawar::Input::IsKeyPressed(M_KEY_W))
+			m_TransformPosition.y += m_TransformMoveSpeed * ts;
 
-		Mawar::RenderCommand::SetClearColor({ clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w });
+		if (Mawar::Input::IsKeyPressed(M_KEY_S))
+			m_TransformPosition.y -= m_TransformMoveSpeed * ts;
+
+		if (Mawar::Input::IsKeyPressed(M_KEY_A))
+			m_TransformPosition.x -= m_TransformMoveSpeed * ts;
+
+		if (Mawar::Input::IsKeyPressed(M_KEY_D))
+			m_TransformPosition.x += m_TransformMoveSpeed * ts;
+
+		Mawar::RenderCommand::SetClearColor({ ClearColor.x * ClearColor.w, ClearColor.y * ClearColor.w, ClearColor.z * ClearColor.w, ClearColor.w });
 		Mawar::RenderCommand::Clear();
 
 		m_Camera.SetPosition(m_CameraPosition);
 		m_Camera.SetRotation(m_CameraRotation);
 
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 		Mawar::Renderer::BeginScene(m_Camera);
-		Mawar::Renderer::Submit(m_Shader, m_SquareVertexArray);
-		Mawar::Renderer::Submit(m_Shader, m_VertexArray);
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				glm::vec3 pos(0.18f * j, 0.18f * i, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Mawar::Renderer::Submit(m_Shader, m_SquareVertexArray, transform);
+			}
+		}
+
+		glm::mat4 transform1 = glm::translate(glm::mat4(1.0f), m_TransformPosition);
+		Mawar::Renderer::Submit(m_Shader, m_VertexArray, transform1);
 		Mawar::Renderer::EndScene();
 	}
 
@@ -137,7 +161,8 @@ public:
 		{
 			ImGui::Begin("Hello World!");
 			ImGui::TextColored(TextCol, "Halo ini window baru dari layer example\npada aplikasi sandbox.");
-			ImGui::ColorEdit3("clear color", (float*)&TextCol);
+			ImGui::ColorEdit3("Text Color", (float*)&TextCol);
+			ImGui::ColorEdit3("Clear Color", (float*)&ClearColor);
 			ImGui::End();
 		}
 	}
@@ -147,7 +172,8 @@ public:
 	}
 
 private:
-	ImVec4 TextCol = ImVec4{ 0.0f,0.0f,0.0f,1.0f };
+	ImVec4 TextCol = ImVec4{ 0.8f,0.8f,0.8f,1.0f };
+	ImVec4 ClearColor = ImVec4{ 0.2f,0.2f,0.2f,1.0f };
 
 	std::unique_ptr<Mawar::Shader> m_Shader;
 	std::shared_ptr<Mawar::VertexArray> m_VertexArray;
@@ -159,6 +185,9 @@ private:
 
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 1.0f;
+
+	glm::vec3 m_TransformPosition;
+	float m_TransformMoveSpeed = 1.0f;
 };
 
 class Sandbox : public Mawar::Application
